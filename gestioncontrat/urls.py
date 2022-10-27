e7a50f9c3f7b8189f2326de3062e2fdf0c02499f
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-10-27 23:19:08 +0000

    add update fonctionality

--- gestioncontrat/urls.py
+++ gestioncontrat/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
         #Leave as empty string for base url
-    path('contrat', views.contrat, name="contrat"), 
-    path('add', views.add, name="add"),
-    path('edit/<int:id>', views.edit, name="edit"),
-    path('delete/<int:id>', views.delete, name="delete"),  
+    path('gestioncontrat/contrat', views.contrat, name="contrat"), 
+    path('gestioncontrat/add', views.add_contrat, name="add_contrat"),
+    path('gestioncontrat/edit/<int:id>', views.edit_contrat, name="edit_contrat"),
+    path('gestioncontrat/delete/<int:id>', views.delete_contrat, name="delete_contrat"),  
 ]
--- gestioncontrat/views.py
+++ gestioncontrat/views.py
@@ -18,7 +18,7 @@ def contrat(request):
     return render(request, 'gestioncontrat/contrat.html', context)
 
 
-def add(request):
+def add_contrat(request):
     if request.method=="POST":
         form = TravailForm(request.POST, request.FILES)
         if form.is_valid():
@@ -31,33 +31,22 @@ def add(request):
         form = TravailForm()
         return render(request, 'gestioncontrat/add.html', {"form":form})
        
-def edit(request):
-    if request.method == "POST":
-        contrat = Travail.objects.get(id = request.POST.get('id'))
-        if contrat != None:
-            contrat.reference = request.POST.get('reference')
-            contrat.objet = request.POST.get('objet')
-            contrat.nature = request.POST.get('nature')
-            contrat.matricule = request.POST.get('matricule')
-            contrat.nom = request.POST.get('nom')
-            contrat.prenom = request.POST.get('prenom')
-            contrat.montant = request.POST.get('montant')
-            contrat.duree = request.POST.get('duree')
-            contrat.date_signature = request.POST.get('date_signature')
-            contrat.date_debut = request.POST.get('date_debut')
-            contrat.date_fin = request.POST.get('date_fin')
-            contrat.nombre_renouv = request.POST.get('nombre_renouv')
-            contrat.date_renouvel = request.POST.get('date_renouvel')
-            contrat.lieu = request.POST.get('lieu')
-            contrat.date_alerte = request.POST.get('date_alerte')
-            contrat.file = request.POST.get('file')
-            contrat.save()
-            messages.success(request, "Contrat updated successfully !")
-            return HttpResponseRedirect("index")
-        
-def delete(request, id):
+def edit_contrat(request, id):
+    contrat = Travail.objects.get(id=id)
+    if request.method == 'POST':
+        form = TravailForm(request.POST,request.FILES, instance=contrat)
+        if form.is_valid():
+            form.save(id)
+            return redirect('contrat')
+    else:
+        form = TravailForm(instance=contrat)
+    return render(request, 'gestioncontrat/edit.html', {'form':form})
+
+
+
+def delete_contrat(request, id):
     contrat = Travail.objects.get(id = id)
     contrat.delete()
     messages.success(request, 'Contrat deleted successfully !')
-    return HttpResponseRedirect("index")
+    return HttpResponseRedirect("contrat")
 # Create your views here.
